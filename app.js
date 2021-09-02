809ffbcdcd9d5fc5548d0f9084c50d6f9b2dab8c
Your Name <you@example.com>
2021-09-02 18:58:47 +0600

    final

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
 const searchBooks = () => {
-    const searchText = document.getElementById('search-field').value;
-    
-    
-    if (searchText ==='') {
-        console.log('write something');
-    }
-    else if (typeof searchText !== 'string'){
-        console.log('ehifbgeifngn');
-    }
-    else {
-        
+    const searchResult = document.getElementById('search-field');
+    const searchText = searchResult.value;
+            
         fetch(`https://openlibrary.org/search.json?q=${searchText}`)
             .then(res => res.json())
             .then(data => displayResult(data))
-    }
-    
+    searchResult.value = '';
 }
 
-
-const displayResult = (books) => {
-    
-    const bookArr = books.docs;
-    
+const displayResult = (books) => {    
+    const bookArr = books.docs;    
     // TOTAL BOOK COUNT 
     const resultCountContainer = document.getElementById('result-count');
     resultCountContainer.innerHTML = `
     <h3>Showing <span>${bookArr.length}</span> of <span>${books.num_found}</span> books</h3>
     `
     //Showing found books
-    
-    
-    bookArr.forEach(book => {
+    if (books.num_found===0) {
+        const error = document.getElementById('error');
+        error.innerText='Please Enter a Valid Name'
+    }
+    else {
+        error.innerText = '';
         const result = document.getElementById('result');
-        const div = document.createElement('div');
-        div.classList.add('col-md-4')
-        div.innerHTML = `
+        result.textContent = '';
+        bookArr.forEach(book => {
+            const div = document.createElement('div');
+            div.classList.add('col-md-4')
+            div.innerHTML = `
         <div class="card "  style="width: 18rem;">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title text-center">${book.title}</h5>
+                <h5 class="card-title text-center">Title: ${book.title}</h5>
                 <p class="card-text text-center">Author: ${book.author_name}</p>
                 <p class="card-text text-center">First Published In: ${book.first_publish_year}</p>  
+                <p class="card-text text-center">Publisher: ${book.publisher}</p>
             </div>
         </div>
         `
-        result.appendChild(div);
-    });
-    
+            result.appendChild(div);
+        });      
+    }
 }
 
